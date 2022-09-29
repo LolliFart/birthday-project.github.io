cf6ffd4e94fe5f58a5bd92d5210cbdce67b5d163
LolliFart <67300397+LolliFart@users.noreply.github.com>
2022-09-30 04:17:42 +0800

    Added new files

---
+++ src/js/index.js
@@ -0,0 +1,61 @@
+
+let firstSlideContainer = 
+document.getElementsByClassName("slide--content")[0];
+
+let secondSlideContainer = 
+document.getElementsByClassName("slide--content--one")[0];
+
+let secondCanvas = 
+document.getElementsByClassName("second--canvas")[0];
+
+secondSlideContainer.setAttribute("style","display:none");
+secondCanvas.setAttribute("style","display:none");
+
+let thirdCanvas = 
+document.getElementsByClassName("third--canvas")[0];
+
+thirdCanvas.setAttribute("style","display:none");
+
+
+
+let containerToggleOne = setTimeout(function(){
+	firstSlideContainer.setAttribute("style","display:none");
+	secondSlideContainer.setAttribute("style","display:block");
+},2500);
+
+
+let removeFirstSlide = setTimeout(function(){
+	document.getElementsByClassName("first--slide")[0].setAttribute("style","display:none;")
+	secondCanvas.setAttribute("style","display:block");
+},6500);
+
+let removeSecondCanvas = setTimeout(function(){
+	secondCanvas.setAttribute("style","display:none");
+	thirdCanvas.setAttribute("style","display:block")
+},9800)
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
